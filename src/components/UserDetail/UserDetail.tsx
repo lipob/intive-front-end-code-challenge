97e945e2cb4877c78b1cf7b8c69afbcaf96deeb9
Leo Blanco <leopoldoblanco@gmail.com>
2022-12-15 00:45:39 -0300

    removed unused vars

--- src/components/UserDetail/UserDetail.tsx
+++ src/components/UserDetail/UserDetail.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { MapPin, Phone, Smartphone, Mail } from 'react-feather'
 import './UserDetail.css'
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,6 @@
-import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/rootReducer'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import { configureStore } from '@reduxjs/toolkit'
 
-// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)))
 const store = configureStore({
   reducer: rootReducer,
 })
